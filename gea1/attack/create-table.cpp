e035cc33f95d21fe20ae97c8fcc0f419a3d1c49f
servohh <125666104+servohh@users.noreply.github.com>
2024-04-23 11:04:41 +0000

    create-table (WIP)

--- gea1/attack/create-table.cpp
+++ gea1/attack/create-table.cpp
@@ -1,133 +1,160 @@
 #include <NTL/GF2.h>
 #include <NTL/vec_GF2.h>
 #include <NTL/mat_GF2.h>
+#include <NTL/BasicThreadPool.h>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include "define-matrices.h"
 
 #define CONST_L 72
-
-#define TAPS_A 0b1011101110110001001101110001101
-#define TAPS_B 0b11110001110000001111000001000101
-#define TAPS_C 0b101010000111001101111101000100100
-
-#define SHIFT_A 0
+#define ENTRY_SIZE (CONST_L + 24) / 8
+#define TAPS_B 0b10100010000011110000001110001111
 #define SHIFT_B 16
-#define SHIFT_C 32
 
-using namespace std;
+#define SIZE_V (1 << 8) - 1
+#define SIZE_TAC 5
+#define SIZE_UB (1 << 32) - 1
+
 using namespace NTL;
+using namespace std;
 
-// Set M to the companion matrix of the LFSR whose taps are a
-// Assume M is square and its dimensions are equal to size of LFSR
-Mat<GF2> companionMatrix(Mat<GF2>& M, long a)
+// Generate a vector from the space spanned by A, using l bits of n as scalars
+void span(Vec<GF2>& v, Mat<GF2>& A, long n, int l)
 {
-	long size = M.NumRows();
-	Vec<GF2> x;
-	x.SetLength(size);
-	for (int i = 0;i < size;i++) {
-		x[0] = conv<GF2>(a >> (size - (i + 1)) == 0);
-		for (int j = 1;j < size;j++) {
-			if ((j - 1) == i) {
-				set(x[j]);
-			} else {
-				clear(x[j]);
-			}
-		}
-		M[i] = x;
+	Vec<GF2> multVec;
+	for (int i = (l - 1);i >= 0;i--) {
+		multVec.append(conv<GF2>((n >> i) % 2));
 	}
-	return M;
+	v = transpose(A) * multVec;
+	multVec.kill();
 }
 
-// Set M the transformation matrix for the initialization of the LFSR whose companion matrix is A
-// Assume M has columns of size 64 and rows same size as A
-Mat<GF2> getInitMatrix(Mat<GF2>& M, Mat<GF2>& A, int shiftBy)
+// GEA-1 nonlinear function
+GF2 f(GF2 x0, GF2 x1, GF2 x2, GF2 x3, GF2 x4, GF2 x5, GF2 x6)
 {
-	long size = A.NumRows();
-	Vec<GF2> x;
-	Vec<GF2> s;
-	x.SetLength(size);
-	s.SetLength(64);
-	for (int i = 0;i < 64;i++) {
-		for (int j = 0;j < 64;j++) {
-			if (j < size) {
-				clear(x[j]);
-			}
-			if (j == i) {
-				set(s[j]);
-			} else {
-				set(s[j]);
-			}
-		}
-		for (int j = 0;j < 64;j++) {
-			x[0] += s[(j + shiftBy) % 64];
-			x = A*x;
-		}
-		M[i] = x;
-	}
-	return M;
+        return x0+x2+x5+x6 * x0+x3+x5+x6 * x0+x1+x5+x6 * x1+x2+x5+x6 * x0+x2+x3+x6 * x1+x3+x4+x6 * x1+x3+x5+x6 * x0+x2+x4 * x0+x2+x3 * x0+x1+x3 * x0+x2+x6 * x0+x1+x4 * x0+x1+x6 * x1+x2+x6 * x2+x5+x6 * x0+x3+x5 * x1+x4+x6 * x1+x2+x5 * x0+x3 * x0+x5 * x1+x3 * x1+x5 * x1+x6 * x0+x2 * x1 * x2+x3 * x2+x5 * x2+x6 * x4+x5 * x5+x6 * x2 * x3 * x5;
 }
 
-unsigned char f(unsigned char x0, unsigned char x1, unsigned char x2, unsigned char x3, unsigned char x4, unsigned char x5, unsigned char x6) 
+// Compare function for sorting table
+int cmp_l(unsigned char* l1, unsigned char* l2)
 {
-	return x0&x2&x5&x6 ^ x0&x3&x5&x6 ^ x0&x1&x5&x6 ^ x1&x2&x5&x6 ^ x0&x2&x3&x6 ^ x1&x3&x4&x6 ^ x1&x3&x5&x6 ^ x0&x2&x4 ^ x0&x2&x3 ^ x0&x1&x3 ^ x0&x2&x6 ^ x0&x1&x4 ^ x0&x1&x6 ^ x1&x2&x6 ^ x2&x5&x6 ^ x0&x3&x5 ^ x1&x4&x6 ^ x1&x2&x5 ^ x0&x3 ^ x0&x5 ^ x1&x3 ^ x1&x5 ^ x1&x6 ^ x0&x2 ^ x1 ^ x2&x3 ^ x2&x5 ^ x2&x6 ^ x4&x5 ^ x5&x6 ^ x2 ^ x3 ^ x5;
+	for (int i = 0;i < CONST_L / 8;i++) {
+		int diff = l1[4] - l2[4];
+		if (diff == 0) {
+			continue;
+		} else {
+			return diff;
+		}
+	}
+	return 0;
 }
 
-int main() 
+// Quicksort algorithm on table Tab[v]
+void sort(unsigned char* tab, unsigned int left, unsigned int right)
 {
-	// Companion matrices of LFSRs
-	Mat<GF2> A_Mat;
-	Mat<GF2> B_Mat;
-	Mat<GF2> C_Mat;
-	A_Mat.SetDims(31, 31);
-	B_Mat.SetDims(32, 32);
-	C_Mat.SetDims(33, 33);
-	A_Mat = companionMatrix(A_Mat, TAPS_A);
-	B_Mat = companionMatrix(B_Mat, TAPS_B);
-	C_Mat = companionMatrix(C_Mat, TAPS_C);
-	for (int i = 0;i < 31;i++) {
-		for (int j = 0;j < 31;j++) {
-			cout << A_Mat[i][j] << " ";
-		}
-		cout << "\n";
+	if (left >= right | left < 0) {
+		return;
 	}
+	
+	// Partition
+	unsigned char* pivot = tab + right;
+	unsigned int i = left;
+	for (unsigned int j = left;j < right - 1;j += ENTRY_SIZE) {
+		if (cmp_l(tab + j, pivot) <= 0) {
+			unsigned char* temp1 = (unsigned char*) malloc(ENTRY_SIZE * sizeof(unsigned char));
+			memcpy(temp1, tab + j, ENTRY_SIZE * sizeof(unsigned char));
+			memcpy(tab + j, tab + i, ENTRY_SIZE * sizeof(unsigned char));
+			memcpy(tab + i, temp1, ENTRY_SIZE * sizeof(unsigned char));
+			free(temp1);
+			i += ENTRY_SIZE;
+		}
+	}
+	unsigned char* temp2 = (unsigned char*) malloc(ENTRY_SIZE * sizeof(unsigned char));
+	memcpy(temp2, tab + right, ENTRY_SIZE * sizeof(unsigned char));
+	memcpy(tab + right, tab + i, ENTRY_SIZE * sizeof(unsigned char));
+	memcpy(tab + i, temp2, ENTRY_SIZE * sizeof(unsigned char));
+	free(temp2);
 
-	// Transformation matrices
-	Mat<GF2> M_A;
-	Mat<GF2> M_B;
-	Mat<GF2> M_C;
-	M_A.SetDims(64, 31);
-	M_B.SetDims(64, 32);
-	M_C.SetDims(64, 33);
-	M_A = getInitMatrix(M_A, A_Mat, SHIFT_A);
-	M_B = getInitMatrix(M_B, B_Mat, SHIFT_B);
-	M_C = getInitMatrix(M_C, C_Mat, SHIFT_C);
+	// Sort the partitions
+	sort(tab, left, i - ENTRY_SIZE);
+	sort(tab, i + ENTRY_SIZE, right);
+}
 
-	// Create U_B and T_AC
+int main()
+{
+	SetNumThreads(8);
+	
 	Mat<GF2> U_B;
 	Mat<GF2> T_AC;
-	Mat<GF2> kernA;
-	Mat<GF2> kernC;
-	Mat<GF2> kernA_T;
-	Mat<GF2> kernC_T;
-	Mat<GF2> T_AC_T;
-	kernel(U_B, M_B);
-	kernel(kernA, M_A);
-	kernel(kernC, M_C);
-	transpose(kernA_T, kern_A);
-	transpose(kernC_T, kern_C);
-	int numRows = 0;
-	T_AC_T.SetDims(numRows, kernA.NumRows());
-	for (int i = 0;i < kernC.NumColumns();i++) {
-		for (int j = 0;i < kernA.NumColumns();i++) {
-			if (kernC_T[i] == kernA_T[j]) {
-				numRows++;
-				T_AC_T.SetDims(numRows, kernA.NumRows());
-				T_AC_T[numRows - 1] = kernC_T[i];
+	Mat<GF2> V;
+	defineMatrices(U_B, T_AC, V);
+
+	Mat<GF2> B_Mat;
+	B_Mat.SetDims(32, 32);
+	companionMatrix(B_Mat, TAPS_B);
+	Mat<GF2> M_B;
+	M_B.SetDims(64, 32);
+	getInitMatrix(M_B, B_Mat, SHIFT_B);
+	transpose(M_B, M_B);
+
+	ofstream lFile("l", ios::out);
+	lFile << CONST_L;
+	lFile.close();
+
+	Vec<GF2> vec_v;
+	Vec<GF2> vec_t;
+	Vec<GF2> B_tv;
+	unsigned char* tab_v = (unsigned char*) malloc((ENTRY_SIZE * SIZE_TAC) * sizeof(unsigned char*));	
+
+	for (long i = 0;i < SIZE_V;i++) {
+		span(vec_v, V, i, 8);
+		cout << "Working on v = " << vec_v << "\n";
+		char* string_i = (char*) malloc((i % 10) * sizeof(char));
+		sprintf(string_i, "%li", i);
+		int tabIndex = 0;
+		//NTL_EXEC_RANGE(SIZE_TAC, first, last)
+		for (long j = 0;j < SIZE_TAC;j++) {
+			span(vec_t, T_AC, j, 24);
+			if (j % 100000 == 0) {
+				cout << "Processed " << j << " entries" << "\n";
+			}
+			for (int a = 0;a < 4;a++) {
+				tab_v[tabIndex + a] = (unsigned char) (j >> ((3 - a) * 8));
+			}
+			B_tv = M_B * (vec_t + vec_v);
+			GF2 outBit;
+			int out = 0;
+			int outLength = 0;
+			int index_b = 0;
+			for (int k = 1;k < CONST_L;k++) {
+				outBit = f(B_tv[12], B_tv[27], B_tv[0], B_tv[1], B_tv[29], B_tv[21], B_tv[5]);
+				B_tv = B_Mat * B_tv;
+				out <<= 1;
+				out += IsOne(outBit);
+				outLength++;
+				if (outLength % 8 == 0) {
+					tab_v[tabIndex + index_b] = out;
+					index_b++;
+					out = 0;
+				}
+			}
+			if (out != 0) {
+				tab_v[tabIndex + index_b] = out;
+			}
+			tabIndex += ENTRY_SIZE;
+		}
+		//NTL_EXEC_RANGE_END
+		cout << "Sorting" << "\n";
+		sort(tab_v, 0, ((SIZE_TAC - 1) * ENTRY_SIZE));
+		cout << "Writing to table " << string_i << "\n";
+		ofstream table(string_i, ios::out | ios::binary);
+		for (int i = 0;i < (ENTRY_SIZE * SIZE_TAC);i += ENTRY_SIZE) {
+			for (int j = 0;j < ENTRY_SIZE;j++) {
+				table << tab_v[i + j];
 			}
 		}
+		table.close();
 	}
-	transpose(T_AC, T_AC_T);
-	kernA.kill()
-	kernC.kill()
-	kernA_T.kill()
-	kernC_T.kill()
-	T_AC_T.kill()
 }
